4992eedcbc70699d18ef326fdcf82d3b48caec08
JuniorCoder <57005033+juniorcoder2008@users.noreply.github.com>
2022-02-22 11:25:46 +0100

    Create typescript-project.cmd

---
+++ typescript-project.cmd
@@ -0,0 +1,1 @@
+git clone https://github.com/juniorcoder2008/typescript-project.git && ren typescript-project %1 && cd %1 && npm install && echo The project was succesfully created && code . && npm run start 
